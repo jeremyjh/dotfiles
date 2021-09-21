2a87396f33a5e78e0c8d94230b8b7cf6f4f58262
Jeremy Huffman <jeremy@jeremyhuffman.com>
2021-09-21 10:36:37 -0400

    track spacemacs-custom.el

---
+++ home/.spacemacs-custom.el
@@ -0,0 +1,73 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" default))
+ '(evil-want-Y-yank-to-eol nil)
+ '(haskell-hoogle-url "https://www.stackage.org/lts/hoogle?q=%s")
+ '(helm-make-cache-targets t)
+ '(package-selected-packages
+   '(string-edit poetry npm-mode multi-line shut-up modus-themes lsp-metals evil-easymotion evil-collection annalist drag-stuff dired-quick-sort cfrs posframe treemacs-icons-dired sphinx-doc ron-mode org-superstar lsp-pyright lsp-origami origami helm-org helm-cider forge closql emacsql-sqlite emacsql treepy emr clang-format list-utils zig-mode treemacs-persp treemacs-magit modus-vivendi-theme modus-operandi-theme magit-section helm-ls-git flycheck-elsa chocolate-theme hybrid-mode dap-mode bui tree-mode reveal-in-osx-finder osx-trash osx-dictionary osx-clipboard launchctl exunit json-snatcher json-reformat epc ctable concurrent deferred parent-mode request dash-docs haml-mode pcache pos-tip let-alist flx iedit paredit anzu scala-mode reformatter all-the-icons memoize autothemer web-completion-data tern dash-functional ghc eclim sesman spinner parseedn parseclj a pkg-info epl f auto-complete popup pcre2el goto-chg undo-tree bind-map writeroom-mode visual-fill-column treemacs-projectile treemacs-evil transient symbol-overlay seeing-is-believing rubocopfmt nodejs-repl lsp-treemacs treemacs pfuture lv lsp-python-ms python lsp-java lsp-haskell helm-lsp helm-git-grep gitignore-templates flycheck-package package-lint evil-textobj-line elm-test-runner doom-modeline shrink-path devdocs blacken attrap dotenv-mode lsp-ui lsp-python lsp-javascript-typescript company-lsp lsp-mode ht overseer nameless macrostep elisp-slime-nav auto-compile packed company-terraform terraform-mode hcl-mode godoctor go-tag go-rename go-impl go-guru go-gen-test go-fill-struct go-eldoc flycheck-gometalinter counsel-gtags company-go go-mode vue-mode edit-indirect ssass-mode vue-html-mode eslint-fix typescript-mode htmlize sbt-mode lcr projectile counsel swiper ivy multiple-cursors anaconda-mode avy company smartparens highlight elixir-mode flycheck haskell-mode yasnippet gitignore-mode window-purpose imenu-list skewer-mode js2-mode simple-httpd magit markdown-mode pythonic rust-mode helm helm-core inf-ruby spaceline powerline s hydra bind-key evil org-plus-contrib pipenv magit-svn magit-popup git-commit ghub async with-editor dash kaolin-themes json-navigator hierarchy helm-xref eziam-theme evil-goggles doom-themes purescript-mode restclient-helm ob-restclient ob-http company-restclient restclient know-your-http-well sayid clojure-snippets clojure-cheatsheet clj-refactor inflections edn peg cider-eval-sexp-fu cider seq queue clojure-mode zenburn-theme zen-and-art-theme yasnippet-snippets yapfify yaml-mode ws-butler winum white-sand-theme which-key web-mode web-beautify volatile-highlights vmd-mode vi-tilde-fringe uuidgen utop use-package underwater-theme ujelly-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme tuareg toxi-theme toml-mode toc-org tide tao-theme tangotango-theme tango-plus-theme tango-2-theme tagedit symon sunny-day-theme sublime-themes subatomic256-theme subatomic-theme string-inflection sql-indent spaceline-all-the-icons spacegray-theme soothe-theme solarized-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme smeargle slim-mode seti-theme scss-mode sass-mode rvm ruby-tools ruby-test-mode ruby-refactor ruby-hash-syntax rubocop rspec-mode robe rjsx-mode reverse-theme restart-emacs rebecca-theme rbenv rake rainbow-delimiters railscasts-theme racer pyvenv pytest pyenv-mode py-isort purple-haze-theme pug-mode psci psc-ide professional-theme prettier-js popwin planet-theme pippel pip-requirements phoenix-dark-pink-theme phoenix-dark-mono-theme persp-mode password-generator paradox organic-green-theme org-bullets open-junk-file omtose-phellack-theme oldlace-theme ocp-indent occidental-theme obsidian-theme ob-elixir noflet noctilux-theme neotree naquadah-theme mvn mustang-theme move-text monokai-theme monochrome-theme molokai-theme moe-theme mmm-mode minitest minimal-theme meghanada maven-test-mode material-theme markdown-toc majapahit-theme magit-gitflow madhat2r-theme lush-theme lorem-ipsum livid-mode live-py-mode linum-relative link-hint light-soap-theme less-css-mode json-mode js2-refactor js-doc jbeans-theme jazz-theme ir-black-theme intero inkpot-theme indent-guide importmagic impatient-mode hy-mode hungry-delete hlint-refactor hl-todo hindent highlight-parentheses highlight-numbers highlight-indentation heroku-theme hemisu-theme helm-themes helm-swoop helm-pydoc helm-purpose helm-projectile helm-mode-manager helm-make helm-hoogle helm-gtags helm-gitignore helm-flx helm-descbinds helm-dash helm-css-scss helm-company helm-c-yasnippet helm-ag hc-zenburn-theme haskell-snippets gruvbox-theme gruber-darker-theme groovy-mode groovy-imports grandshell-theme gradle-mode gotham-theme google-translate golden-ratio gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md ggtags gandalf-theme fuzzy font-lock+ flycheck-rust flycheck-pos-tip flycheck-ocaml flycheck-mix flycheck-haskell flycheck-elm flycheck-credo flx-ido flatui-theme flatland-theme fill-column-indicator farmhouse-theme fancy-battery eyebrowse expand-region exotica-theme exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu espresso-theme erlang ensime emmet-mode elm-mode editorconfig dumb-jump dtrt-indent dracula-theme django-theme diminish define-word dash-at-point darktooth-theme darkokai-theme darkmine-theme darkburn-theme dante dakrone-theme cython-mode cyberpunk-theme csv-mode counsel-projectile company-web company-tern company-statistics company-ghci company-ghc company-emacs-eclim company-cabal company-anaconda column-enforce-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized coffee-mode cmm-mode clues-theme clean-aindent-mode chruby cherry-blossom-theme centered-cursor-mode cargo busybee-theme bundler bubbleberry-theme birds-of-paradise-plus-theme badwolf-theme auto-yasnippet auto-highlight-symbol apropospriate-theme anti-zenburn-theme ample-zen-theme ample-theme alect-themes alchemist aggressive-indent afternoon-theme adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell))
+ '(projectile-globally-ignored-directories
+   '("node_modules" "bower_components" ".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work"))
+ '(psc-ide-add-import-on-completion t t)
+ '(psc-ide-rebuild-on-save nil t)
+ '(purescript-indent-spaces 2)
+ '(safe-local-variable-values
+   '((eval progn
+           (add-to-list 'exec-path
+                        (concat
+                         (locate-dominating-file default-directory ".dir-locals.el")
+                         "frontend/node_modules/.bin/")))
+     (eval progn
+           (puthash "dialyzerEnabled" :json-false lsp-elixir--config-options)
+           (add-to-list 'exec-path
+                        (concat
+                         (locate-dominating-file default-directory ".dir-locals.el")
+                         "assets/node_modules/.bin/")))
+     (go-backend . go-mode)
+     (go-backend . lsp)
+     (intero-targets "advent:lib" "advent:exe:advent-2018-exe" "advent:test:advent-2018-test")
+     (intero-targets "college-vine:lib" "college-vine:exe:college-vine-exe" "college-vine:test:college-vine-test")
+     (eval progn
+           (spacemacs|disable-company elixir-mode)
+           (add-to-list 'exec-path
+                        (concat
+                         (locate-dominating-file default-directory ".dir-locals.el")
+                         "assets/node_modules/.bin/")))
+     (intero-targets "devhook-be-api:lib" "devhook-be-api:exe:dh-be-api-svr" "devhook-be-api:exe:dh-be-api-tasks" "devhook-be-api:test:be-api-test" "devhook-be-api:bench:be-api-bench")
+     (intero-targets "devhook-be-api:lib" "devhook-be-api:exe:be-api-svr" "devhook-be-api:exe:be-api-tasks" "devhook-be-api:test:be-api-test" "devhook-be-api:bench:be-api-bench")
+     (intero-targets "be-api:lib" "be-api:exe:be-api-svr" "be-api:exe:be-api-tasks" "be-api:test:be-api-test" "be-api:bench:be-api-bench")
+     (intero-targets "devhook:lib" "devhook:exe:devhook-tasks" "devhook:exe:devhook-web" "devhook:test:devhook-test" "devhook:bench:devhook-bench")
+     (intero-targets "devhook:lib" "devhook:exe:devhook-develmain" "devhook:exe:devhook-psgen" "devhook:exe:devhook-web" "devhook:test:devhook-test" "devhook:bench:devhook-bench")
+     (intero-targets "devhook:lib" "devhook:exe:devhook-psgen" "devhook:exe:devhook-web" "devhook:test:devhook-test" "devhook:bench:devhook-bench")
+     (intero-targets "hs-duktape:lib" "hs-duktape:test:tests")
+     (intero-targets "devhook:lib" "devhook:exe:devhook-bench" "devhook:exe:devhook-devel" "devhook:exe:devhook-psgen" "devhook:exe:devhook-web" "devhook:test:devhook-test")
+     (intero-targets "devhook:lib" "devhook:exe:devhook-devel" "devhook:exe:devhook-psgen" "devhook:exe:devhook-web" "devhook:test:devhook-test")
+     (intero-targets "hs-duktape:lib")
+     (typescript-backend . tide)
+     (typescript-backend . lsp)
+     (javascript-backend . tern)
+     (javascript-backend . lsp)
+     (intero-targets "cronulent:lib" "cronulent:exe:cronulent-devel" "cronulent:exe:cronulent-psgen" "cronulent:test:cronulent-test")
+     (intero-targets "cronulent:lib" "cronulent:exe:cronulent-devel" "cronulent:test:cronulent-test")
+     (intero-targets "cronulent:lib" "cronulent:test:cronulent-test")
+     (intero-targets "cronulent:lib" "cronulent:exe:cronulent-devel")
+     (haskell-process-use-ghci . t)
+     (haskell-indent-spaces . 4)
+     (intero-targets "distributed-process-lifted:lib" "distributed-process-lifted:test:testlifted")
+     (eval progn
+           (add-to-list 'exec-path
+                        (concat
+                         (locate-dominating-file default-directory ".dir-locals.el")
+                         "assets/node_modules/.bin/")))
+     (elixir-enable-compilation-checking . t)
+     (elixir-enable-compilation-checking))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
