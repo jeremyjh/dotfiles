512ca7ce7565d1c4853da27b9a05d9f5e59e4b20
Jeremy Huffman <jeremy@jeremyhuffman.com>
2023-03-23 08:09:14 -0400

    add test_iex in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# install.sh will be invoked by Codespaces after cloning into a new space
+# install.sh will be automatically invoked by Codespaces after cloning into a new space
 
 set -e
 
@@ -11,3 +11,5 @@ source $HOME/.homesick/repos/homeshick/homeshick.sh
 homeshick clone -b jeremyjh/dotfiles
 
 homeshick link -b dotfiles
+
+git clone -b scott https://github.com/scottming/test_iex ~/.test_iex
